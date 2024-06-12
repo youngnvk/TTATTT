6f12eab7d047c90fa2b86a4d08b5e1ec38b7071a
youngnvk <toilakhai18@gmail.com>
2024-06-12 15:23:11 +0700

    Lastly

--- bai41.py
+++ bai41.py
@@ -6,16 +6,28 @@ def checknto(n):
         if n % i == 0:
             return False
     return True
-def binhphuongcolap(a, k, n):
-    if a % n == 0:
-        return 0
+def bin(n):  # Chuyển đổi số nguyên sang dạng nhị phân
+    arr2 = []
+    cnt = 0
+    while n != 0:
+        r = n % 2
+        arr2.append(r)
+        cnt += 1
+        n = n // 2
+    return cnt, arr2
+
+def modulo(a, k, n):  # Hàm tính lũy thừa modulo bằng phương pháp bình phương và nhân
+    cnt, arr2 = bin(k)
     b = 1
+    if k == 0:
+        return 1  # 0^0 là 1 theo định nghĩa
     A = a
-    while(k != 0):
-        if k % 2 == 1:
+    if arr2[0] == 1:
+        b = a 
+    for i in range(1, cnt):  # Bắt đầu từ phần tử thứ hai
+        A = (A * A) % n
+        if arr2[i] == 1:
             b = (A * b) % n
-        A = (a * A) % n
-        k = k // 2
     return b
 if __name__ == '__main__':
     a = int(input('Nhap a: '))
