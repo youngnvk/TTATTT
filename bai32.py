cb321704d92023b2cf00b903583ecd45954de93c
youngnvk <toilakhai18@gmail.com>
2024-06-12 15:25:33 +0700

    Lastly

--- bai32.py
+++ bai32.py
@@ -20,8 +20,29 @@ def compute_phi(p, q):
 def compute_d(e, phi):
     return modulo(e, -1, n)
 
-def modulo(a, k, n):
-    if a % m == 0:
+def bin(n):  # Chuyển đổi số nguyên sang dạng nhị phân
+    arr2 = []
+    cnt = 0
+    while n != 0:
+        r = n % 2
+        arr2.append(r)
+        cnt += 1
+        n = n // 2
+    return cnt, arr2
+
+def modulo(a, k, n):  # Hàm tính lũy thừa modulo bằng phương pháp bình phương và nhân
+    cnt, arr2 = bin(k)
+    b = 1
+    if k == 0:
+        return 1  # 0^0 là 1 theo định nghĩa
+    A = a
+    if arr2[0] == 1:
+        b = a 
+    for i in range(1, cnt):  # Bắt đầu từ phần tử thứ hai
+        A = (A * A) % n
+        if arr2[i] == 1:
+            b = (A * b) % n
+    return b    if a % m == 0:
         return 0
     b = 1
     A = a
