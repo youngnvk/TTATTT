e480f002b0c10b9336e5cf59c8f0a438eb7e79a1
youngnvk <toilakhai18@gmail.com>
2024-06-12 15:56:53 +0700

    Lastly

--- bai3.py
+++ bai3.py
@@ -1,5 +1,5 @@
 import math
-def phantichNto(N, A):
+def phantichNto(N, A): #Ham tach uoc nguyen to
     limit = int(math.sqrt(N)) + 1
     for i in range(2, limit, 1):
         if N % i == 0:
@@ -16,7 +16,12 @@ def phantich(N, B):
             if i * i != N:
                 B.append(N // i)
 if __name__=="__main__":
-    N = int(input("Nhập giá trị N: "))
+    while(True):
+        N = int(input("Nhập giá trị N nguyên dương: "))
+        if N > 0:
+            break
+        else:
+            print('Mời bạn nhập lại n!')
     A = []
     B = []
     phantichNto(N, A)
@@ -25,7 +30,11 @@ if __name__=="__main__":
     k = len(A)
     p = sum(B)
     s = len(B)
+    print(f'q = {q}')
+    print(f'k = {k}')
+    print(f'p = {p}')
+    print(f's = {s}')
     result = N + p + s - q - k
-    print("Kết quả của biểu thức N+p+s-q-k với N =", N, "là:", result)
+    print("Kết quả của biểu thức N + p + s - q - k với N =", N, "là:", result)
     
     
