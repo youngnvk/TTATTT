2590c86224e019bb6036d3a1cb979ffe53192ef5
youngnvk <toilakhai18@gmail.com>
2024-06-12 16:46:22 +0700

    Lastly

--- bai7.py
+++ bai7.py
@@ -1,13 +1,12 @@
-
 import math
-def rivot(N):
+def reversed(N): #hàm đảo ngược 1 số
     reverse = 0
     while(N != 0):
         r = N  % 10
-        N //= 10
         reverse = reverse * 10 + r
+        N //= 10
     return reverse
-def checkNto(N):
+def checkNto(N): #hàm check nguyên tố
     if N < 2:
         return False
     for i in range(2, int(math.sqrt(N)) + 1, 1):
@@ -15,12 +14,16 @@ def checkNto(N):
             return False
     return True  
 if __name__ == '__main__':
-    N = int(input('Nhập số nguyên N : '))
+    while(True):
+        N = int(input('Nhập số nguyên N > 0: '))
+        if N > 0:
+            break
+        else:
+            print('Nhập lại!')
+    cnt = 1
     for i in range(10, N, 1):
-        if checkNto(i) == True and checkNto(rivot(i)) == True and i != rivot(i):
-            print(i)
-            
-        
-        
-    
-    
+        if checkNto(i) == True and checkNto(reversed(i)) == True and i != reversed(i):
+            print(f'số emirp thứ {cnt} là: ', i)
+            cnt += 1
+    if cnt == 0:
+        print('Không có số nào !')
