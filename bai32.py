d7e1f7927e730d476d907cdc19aa32809f34101c
youngnvk <toilakhai18@gmail.com>
2024-06-17 21:44:40 +0700

    Lastly

--- bai32.py
+++ bai32.py
@@ -1,66 +1,108 @@
 import random
 import math
-def is_prime(num):
-    if num < 2:
+import sys
+def is_prime(n):
+    if n < 2:
         return False
-    for i in range(2, int(math.sqrt(num)) + 1):
-        if num % i == 0:
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
             return False
     return True
 
-def random_prime():
+def random_prime(n): #random ra 1 so nguyen to trong khoang 101-499
     while True:
-        p = random.randint(101, 499)
+        p = random.randint(101, n)
         if is_prime(p):
-            return p
+            break
+    return p
 
-def tinh_phi(p, q):
+def tinh_phi(p, q): #Tính phi(n) = (p - 1).(q - 1)
     return (p - 1) * (q - 1)
 
 def tinh_d(e, phi):
-    return modulo(e, -1, n)
+        return nghichdao(e, phi)
+def gcd(a, b):
+    A = a
+    B = b
+    while(B > 0):
+        R = A % B
+        A = B
+        B = R
+    return A
+
+def nghichdao(a, p):
+    if gcd(a, p) != 1: # điều kiện nghịch đảo
+        return False
+    u = a
+    v = p
+    x1 = 1
+    x2 = 0
+    if u == 0: #không thể chia cho 0
+        return False
+    while(u != 1):
+        q = v // u
+        r = v - q * u
+        x = x2 - q * x1
+        v = u 
+        u = r
+        x2 = x1
+        x1 = x
+    return x1 % p
 
 def bin(n):  # Chuyển đổi số nguyên sang dạng nhị phân
-    arr2 = []
+    arr = []
     cnt = 0
     while n != 0:
         r = n % 2
-        arr2.append(r)
+        arr.append(r)
         cnt += 1
         n = n // 2
-    return cnt, arr2
+    return cnt, arr
 
 def modulo(a, k, n):  # Hàm tính lũy thừa modulo bằng phương pháp bình phương và nhân
-    cnt, arr2 = bin(k)
+    cnt, arr = bin(k)
     b = 1
     if k == 0:
-        return 1  # 0^0 là 1 theo định nghĩa
+        return b  # 0^0 là 1 theo định nghĩa
     A = a
-    if arr2[0] == 1:
+    if arr[0] == 1:
         b = a 
     for i in range(1, cnt):  # Bắt đầu từ phần tử thứ hai
         A = (A * A) % n
-        if arr2[i] == 1:
+        if arr[i] == 1:
             b = (A * b) % n
     return b      
-def encrypt(m, e, n):
+def mahoa(m, e, n):
     return modulo(m, e, n)
 
-def decrypt(c, d, n):
+def giaima(c, d, n):
     return modulo(c, d, n)
-
+def tim_e(n):
+    while(True):
+        e = random.randint(1, phi - 1)
+        if gcd(e, n) == 1:
+            break
+    return e
 if __name__ == '__main__':
-    p, q = random_prime(), random_prime()
+    n = 500
+    p = random_prime(n)
+    q = random_prime(n)
+    print(f"2 số nguyên tố p và q chọn được trong khoảng (100->500) là : {p} và {q}")
     n = p * q
+    print(f"giá trị của n = {p} * {q} là : {n}")
     phi = tinh_phi(p, q)
-    e = random.randint(2, phi - 1)
+    print(f"giá trị của phi({n}) = {phi}")
+    e = tim_e(phi)
+    print(f"giá trị e nguyên tố cùng với phi(n) là e = {e}")
     d = tinh_d(e, phi)
-
-    SBD = int(input('Nhap ma sinh vien : '))
+    if d == False:
+        print(f'Không tìm thấy giá trị d là nghich đảo của e ({e}^-1 mod {n})')
+        sys.exit() # kết thúc chương trình
+    else:
+        print(f"giá trị của d là : {d}")
+    SBD = int(input('Nhap So Bao Danh : '))
     m = SBD + 123
-
-    c = encrypt(m, e, n)
-    print(f'Ma hoa : (c): {c}')
-
-    decrypted_m = decrypt(c, d, n)
-    print(f'Giai ma : (m): {decrypted_m}')
+    ma_hoa = mahoa(m, e, n)
+    print(f'Ma hoa (c) : {ma_hoa}')
+    giai_ma = giaima(ma_hoa, d, n)
+    print(f'Giai ma (m) : {giai_ma}')
