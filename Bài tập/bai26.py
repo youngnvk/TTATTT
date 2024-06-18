a8391b1df15f7b7fe3c9cc3bd1f52a12a62f8ba4
youngnvk <toilakhai18@gmail.com>
2024-06-18 20:11:48 +0700

    Last.ly

--- Bài tập/bai26.py
+++ Bài tập/bai26.py
@@ -1,5 +1,5 @@
-
 import math
+
 def eratosthenes(n):
     primes = [1] * (n + 1)
     primes[0] = primes[1] = 0
@@ -8,20 +8,22 @@ def eratosthenes(n):
             for j in range(i * i, n + 1, i):
                 primes[j] = 0
     return [i for i in range(2, n + 1) if primes[i]]
-def check(N): #hàm kiểm tra theo yêu cầu bài toán
+
+def check(N):
     primes = eratosthenes(N)
     for i in primes:
         if N % i == 0:
             if N % (i * i) != 0:
                 return False
     return True
-if __name__=='__main__':
-    N = 20 #cho trước < 10000
+
+if __name__ == '__main__':
+    N = 20  # cho trước < 10000
     cnt = 1
-    for i in range(2, N): #kiểm tra những số nhỏ hơn n
+    for i in range(2, N):
         result = check(i)
         if result:
-            print(f'Đây là số mạnh mẽ hơn số {N} thứ {cnt} : ', i)
+            print(f'Đây là số mạnh mẽ thứ {cnt} nhỏ hơn {N}:', i)
             cnt += 1
     if cnt == 1:
-        print(f'Không có số nào mạnh hơn {N}')  
+        print(f'Không có số nào mạnh hơn {N}')
